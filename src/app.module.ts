503dbe6ffb8bcd98e38bf3037068038e0eccd41d
Chinemelum <chinemelum.elomba@kuda.com>
2025-03-19 11:08:53 +0100

    Update route wildcard, create database schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,6 @@ import { LoggerMiddleware } from './common/middleware/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(LoggerMiddleware).forRoutes('*path');
   }
 }
