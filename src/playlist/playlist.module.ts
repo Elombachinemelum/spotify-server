99ae3ad995d8025af7254af5f179cffa64a1a4e8
Chinemelum <chinemelum.elomba@kuda.com>
2025-03-25 00:37:48 +0100

    Remove unecesary service provider registration

--- src/playlist/playlist.module.ts
+++ src/playlist/playlist.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { PlaylistController } from './playlist.controller';
 import { PlaylistService } from './playlist.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { UserService } from 'src/user/user.service';
 import { UserModule } from 'src/user/user.module';
 
 @Module({
   controllers: [PlaylistController],
-  providers: [PlaylistService, UserService],
+  providers: [PlaylistService],
   imports: [PrismaModule, UserModule],
 })
 export class PlaylistModule {}
